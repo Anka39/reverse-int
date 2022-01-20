8c400a944d17fc8116ab5c978413397b438e241d
Anka39 <86690194+Anka39@users.noreply.github.com>
2022-01-20 23:32:24 +0300

    feat: complite task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function reverse (n) {
-  
+    const abs = Math.abs(n);
+
+    const str = abs.toString();
+
+    let result = '';
+
+  for(let i = str.length - 1; i >= 0; i--){
+    result = result + str[i];
+  };
+
+  return Number(result);
 }
